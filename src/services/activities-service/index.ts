e0ccdb9e5bfa2898b19b35233b34b461b77d5319
Analice <anadias.alice@gmail.com>
2023-03-17 13:27:57 -0300

    feat: +subscribe +deleteSubscribe +updateActivityCapacity +userActivityByActivityId +userActivities

--- src/services/activities-service/index.ts
+++ src/services/activities-service/index.ts
@@ -5,15 +5,45 @@ async function eventDays(userId: number) {
 
 async function activitiesByDayId(dayId: number) {
   const activities = activitiesRepository.findActivitiesByDayId(dayId);
+  return activities;
 }
 
-async function subscribe(userId: number) {
+async function userActivityByActivityId(userId: number, activityId: number) {
+  const activities = await activitiesRepository.findUserActivityByActivityId(userId, activityId);
+  if(activities.length === 0) return false;
+  return activities;
+}
+
+async function userActivities(userId: number) {
+  const activities = await activitiesRepository.findUserActivitiesByUserId(userId);
+  return activities;
+}
+
+async function subscribe(userId: number, activityId: number) {
+  const newSubscribe = await activitiesRepository.subscribe(userId, activityId);
+  const newCapacity = newSubscribe.Activities.capacity - 1;
+  await updateActivityCapacity(newSubscribe.Activities.id, newCapacity);
+  return newSubscribe;
+}
+
+async function deleteSubscribe(subscriptionId: number) {
+  const delSubscribe = await activitiesRepository.deleteSubscribe(subscriptionId);
+  const newCapacity = delSubscribe.Activities.capacity + 1;
+  await updateActivityCapacity(delSubscribe.Activities.id, newCapacity);
+  return delSubscribe;
+}
+
+async function updateActivityCapacity(activityId: number, newCapacity: number) {
+  await activitiesRepository.updateActivityCapacity(activityId, newCapacity);
 }
 
 const activitiesService = {
   eventDays,
   activitiesByDayId,
+  userActivityByActivityId,
+  userActivities,
   subscribe,
+  deleteSubscribe,
 };
 
 export default activitiesService;
